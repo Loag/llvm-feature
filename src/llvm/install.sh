25745e37da7e7f18f554ca702661b8327de4582a
Logan <10239411+Loag@users.noreply.github.com>
2023-07-28 22:51:18 -0400

    fix script

--- src/llvm/install.sh
+++ src/llvm/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt install lsb-release wget software-properties-common gnupg
+apt update && apt install lsb-release wget software-properties-common gnupg -y -qq
 
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
